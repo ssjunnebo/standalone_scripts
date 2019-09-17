8c6d4629bb11e995f4f4697d492f67f6a55cc959
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2019-09-17 16:17:48 +0200

    write tsv files

--- start_sarek.py
+++ start_sarek.py
@@ -23,8 +23,8 @@ def start_sarek(project_id, gender, sample_list, no_submit_jobs):
         sample_status = check_analysis_status(sample)
         if sample_status == 'TO_ANALYZE':
             sample_data_paths = get_fastq_files(project_id, sample)
-            if fastq_files_exist(sample_data_paths):
-                make_tsv(sample, gender, project_id, sample_data_paths)
+            if sample_data_paths:
+                tsv_file = make_tsv(sample, gender, project_id, sample_data_paths)
                 make_sbatch_script(sample)
                 if not no_submit_jobs:
                     submit_sbatch_job(sample)   #Should catch the output
@@ -40,12 +40,12 @@ def start_sarek(project_id, gender, sample_list, no_submit_jobs):
 
 def get_samples_from_charon(pid):
     """TODO: Given a project ID, get a list of samples from Charon"""
-    samples = ['P9451_401', 'P9451_402']
+    samples = ['P9451_401', 'P9451_402'] #TODO: check if project and sample exists
     return samples
 
 def check_analysis_status(sample):
     """TODO: Given a sample id, connect to charon and return the analysis status"""
-    if sample == 'P9451_401':
+    if sample == 'P9451_401' or sample == 'P9451_402':
         analysis_status = "TO_ANALYZE"
     else:
         analysis_status = "ANALYSED"
@@ -53,21 +53,20 @@ def check_analysis_status(sample):
 
 def get_fastq_files(pid, sampleid):
     """Given a project and sample ID, return a list of paths to the fastq files. If the files don't exist, an empty list is returned."""
-    path_pattern = os.path.join('/Users/sara.sjunnebo/code/scratch/DATA', pid, sampleid, '*/*/*.gz') # Assuming R1 and R2 are the only .gz files in the data dir
+    path_pattern = os.path.join('/Users/sara.sjunnebo/code/scratch/DATA', pid, sampleid, '*/*/*R1*.gz') # Assuming R1 and R2 are the only .gz files in the data dir TODO: get path from config file?
     sample_fastq_paths = glob.glob(path_pattern)
     return sample_fastq_paths
 
-def fastq_files_exist(files):
-    """Given a list of paths, check if there are two paths"""
-    if len(files) != 2:
-        return False
-    else:
-        return True
-
-def make_tsv(sample, gender, project_id, sample_fastq_paths):
-    """TODO: Given a sample and a project ID, generate a tsv file for input to Sarek"""
-    print('Writing tsvfile for ' + sample)
-    return
+def make_tsv(sample, gender, project, sample_fastq_paths):
+    """Given sample information, generate a tsv file for input to Sarek"""
+    for index, frw_fastq in enumerate(sample_fastq_paths, 1):
+        rev_fastq = frw_fastq.replace('_R1_','_R2_')
+        lane_nr = str(index)
+        sample_tsv = sample + '.tsv'
+        tsv_file_path = os.path.join('/Users/sara.sjunnebo/code/scratch/ANALYSIS', project, 'sarek_ngi', sample, sample_tsv) #TODO: create dir if it doesn't exist TODO: get path from config file?
+        with open(tsv_file_path, 'a') as f:
+            f.write(sample + '\t' + gender + '\t' + '0' + '\t' + sample + '\t' + sample + '_' + lane_nr + '\t' + frw_fastq + '\t' + rev_fastq + '\n')   #TODO: Check if file exists already and prompt if overwrite
+    return tsv_file_path
 
 def make_sbatch_script(sample):
     """TODO: Given a sample, make the sbatch script to start a Sarek run"""
