f25f96b428cc115988f45c1e330d410e1c3b716b
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2019-09-17 14:52:28 +0200

    get and check fastq separate functions

--- start_sarek.py
+++ start_sarek.py
@@ -21,17 +21,21 @@ def start_sarek(project_id, gender, sample_list, no_submit_jobs):
 
     for sample in samples_to_analyse:
         sample_status = check_analysis_status(sample)
-        if sample_status == 'TO_ANALYZE' and fastq_exists(project_id, sample):
-            make_tsv(sample, gender, project_id)
-            make_sbatch_script(sample)
-            if not no_submit_jobs:
-                submit_sbatch_job(sample)   #Should catch the output
-                update_analysis_status(sample)
+        if sample_status == 'TO_ANALYZE':
+            sample_data_paths = get_fastq_files(project_id, sample)
+            if fastq_files_exist(sample_data_paths):
+                make_tsv(sample, gender, project_id, sample_data_paths)
+                make_sbatch_script(sample)
+                if not no_submit_jobs:
+                    submit_sbatch_job(sample)   #Should catch the output
+                    update_analysis_status(sample)
+                else:
+                    print("Generated files. Not submitting jobs.")
+                    break
             else:
-                print("Generated files. Not submitting jobs.")
-                break
+                print("Issue locating fastq files - Not analyzing sample: " + sample) #TODO: Log/warn this
         else:
-            print("NOT ANALYZING: " + sample)
+            print("Sample status not 'TO_ANALYZE' - Not analysing sample: " + sample)
 
 
 def get_samples_from_charon(pid):
@@ -47,17 +51,20 @@ def check_analysis_status(sample):
         analysis_status = "ANALYSED"
     return analysis_status
 
-def fastq_exists(pid, sample):
-    """Given a sample, check if the fastq files exist"""
-    path_pattern = os.path.join('/Users/sara.sjunnebo/code/scratch/DATA', pid, sample, '*/*/*.gz') # Assuming R1 and R2 are the only .gz files in the data dir
-    paths = glob.glob(path_pattern)
-    if not paths:
-        print("Can't find fastq files for sample " + sample) #TODO: Log/warn this
+def get_fastq_files(pid, sampleid):
+    """Given a project and sample ID, return a list of paths to the fastq files. If the files don't exist, an empty list is returned."""
+    path_pattern = os.path.join('/Users/sara.sjunnebo/code/scratch/DATA', pid, sampleid, '*/*/*.gz') # Assuming R1 and R2 are the only .gz files in the data dir
+    sample_fastq_paths = glob.glob(path_pattern)
+    return sample_fastq_paths
+
+def fastq_files_exist(files):
+    """Given a list of paths, check if there are two paths"""
+    if len(files) != 2:
         return False
     else:
         return True
 
-def make_tsv(sample, gender, project_id):
+def make_tsv(sample, gender, project_id, sample_fastq_paths):
     """TODO: Given a sample and a project ID, generate a tsv file for input to Sarek"""
     print('Writing tsvfile for ' + sample)
     return
