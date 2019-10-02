0171bdc3fd9c45c615f0017f263dd980e2a51941
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2019-10-02 15:38:28 +0200

    Don't update local tracking database for now

--- start_sarek/start_sarek.py
+++ start_sarek/start_sarek.py
@@ -3,50 +3,34 @@ from ngi_pipeline.database.classes import CharonSession, CharonError
 from ngi_pipeline.utils.slurm import get_slurm_job_status
 from ngi_pipeline.utils.slurm import get_slurm_job_status
 from ngi_pipeline.utils.config import load_yaml_config, locate_ngi_config
-from ngi_pipeline.engines.sarek.process import ProcessConnector
-from ngi_pipeline.engines.piper_ngi.database import get_db_session, SampleAnalysis
 import os
 import re
 import glob
 import click
 import subprocess
-import time
-import contextlib
 
 
 @click.command()
 @click.option('--gender', type=click.Choice(['XX', 'XY']),
               required=True,
               help="Gender of sample")
-@click.option('--sample_list', type=click.Path(exists=True), default=None,
-              help="Optional list of sample ids to include in the analysis run")
-@click.option('--no_submit_jobs', is_flag=True,
-              help="Only generate the tsv files and sbatch scripts")
-@click.option('--mode', type=click.Choice(['germline', 'somatic']),
-              help="Run Sarek germline or somatic") # TODO: Make this required once implemented
 @click.option('--genome', type=click.Choice(['GRCh37', 'GRCh38']),
               required=True,
               help="Run Sarek with GRCh37 or GRCh38")
+@click.option('--no_submit_jobs', is_flag=True,
+              help="Only generate the tsv files and sbatch scripts")
 @click.argument('project_id')
 
 
-def start_sarek(project_id, gender, sample_list, no_submit_jobs, mode, genome):
+def start_sarek(project_id, gender, genome, no_submit_jobs):
     """Given a project ID, launch Sarek analysis for all samples in that project
     with analysis status set to 'TO_ANALYZE' in Charon.
     """
     charon_connection = CharonSession()
-    sample_entries = charon_connection.project_get_samples(project_id).get("samples", {})   # Returns a list with one dict per sample
+    sample_entries = charon_connection.project_get_samples(project_id).get("samples", {})
     config = load_yaml_config(locate_ngi_config())
-    project_base_path = config['start_sarek']['project_base_path'] # TODO: should this be the path for the uppmax project or the analysis project?
-    ngi_project_id = config['environment']['project_id']
+    project_base_path = config['start_sarek']['project_base_path']
     template_path = config['start_sarek']['sbatch_template']
-
-#TODO: add option sample_list that can filter out specific samples to run analysis for from a list provided. if sample_name in samples_to_analyse
-#    if sample_list:
-#        with open(sample_list, 'r') as sample_input_file:
-#            samples_to_analyse = sample_input_file.read().splitlines()
-#    else:
-#        samples_to_analyse = get_samples_from_charon(sample_entries)
 
     for sample_entry in sample_entries:
         sample_name = sample_entry.get("sampleid")
@@ -54,7 +38,7 @@ def start_sarek(project_id, gender, sample_list, no_submit_jobs, mode, genome):
         if sample_status == 'TO_ANALYZE':
             sample_data_paths = get_fastq_files(project_base_path, project_id, sample_name)
 
-            if sample_data_paths: # TODO: this will only indicate if the R1 files exsist. Should also look for the R2 files somewhere
+            if sample_data_paths:    # TODO: this will only indicate if the R1 files exsist. Should also look for the R2 files somewhere
                 analysis_path = os.path.join(project_base_path, 'ANALYSIS', project_id, 'sarek_ngi', sample_name)
                 make_analysis_dir(analysis_path)
 
@@ -69,41 +53,14 @@ def start_sarek(project_id, gender, sample_list, no_submit_jobs, mode, genome):
                     break
                 else:
                     job_id = submit_sbatch_job(sample_name, analysis_path, sbatch_file_path)
-                    print("Sarek job " + str(job_id) + " submitted for sample " + sample_name) # TODO: Log this
+                    print("Sarek job " + str(job_id) + " submitted for sample " + sample_name)
                     charon_connection.sample_update(project_id, sample_name, analysis_status='UNDER_ANALYSIS')
-                    print("Updated analysis status in charon for " + sample_name) # TODO: Log this and add check that the status was updated
-
-                    # Update local tracking database with jobinfo
-                    time.sleep(5)
-
-                    db_obj = SampleAnalysis(
-                        project_id=ngi_project_id,
-                        project_name=ngi_project_id,
-                        sample_id=sample_name,
-                        project_base_path=project_base_path,
-                        workflow='SarekGermlineAnalysis',  # TODO: implement for somatic later
-                        engine='sarek',
-                        analysis_dir=analysis_path,
-                        **{'slurm_job_id': job_id})
-
-                    with database_session(config) as db_session:
-                        db_session.add(db_obj)
-                        db_session.commit()
+                    print("Updated analysis status in charon for " + sample_name)
 
             else:
-                print("Issue locating fastq files - Not analyzing sample: " + sample_name) # TODO: Log/warn this
+                print("Issue locating fastq files - Not analyzing sample: " + sample_name)
         else:
             print("Sample status not 'TO_ANALYZE' - Not analysing sample: " + sample_name)
-
-
-#def get_samples_from_charon(sample_entries_from_charon):
-#    """Given a project ID, get a list of samples from Charon"""
-#    samples = []
-#    for sample_entry in sample_entries_from_charon:
-#        sample_name = sample_entry.get("sampleid")
-#        samples.append(sample_name)
-#
-#    return samples
 
 
 def get_fastq_files(project_path, project_id, sample_id):
@@ -130,7 +87,7 @@ def make_tsv(sample, gender, project, sample_fastq_paths, tsv_file_path):
                         + sample + '_' + lane_nr + '\t'
                         + frw_fastq + '\t'
                         + rev_fastq + '\n')
-    print('Writing tsv file in ' + tsv_file_path) # TODO: log instead
+    print('Writing tsv file in ' + tsv_file_path)
 
     return
 
@@ -164,25 +121,15 @@ def submit_sbatch_job(sample, analysis_dir, sbatch_path):
                      "-J", sample + "_sarek",
                      "-e", os.path.join(analysis_dir, sample + "_sarek.err"),
                      "-o", os.path.join(analysis_dir, sample + "_sarek.out"),
-                    sbatch_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE) # sbatch script already contains pid and sample id
+                    sbatch_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     process_out, process_err = process_handle.communicate()
-    print("Submitting sbatch job for " + sample) # TODO: log this and also print the job id
     try:
         slurm_job_id = re.match(r'Submitted batch job (\d+)', process_out).groups()[0]
     except AttributeError:
         raise RuntimeError('Could not submit sbatch file "{}": '
                            '{}'.format(sbatch_path, process_err))
     return int(slurm_job_id)
-
-@contextlib.contextmanager
-def database_session(conf):
-    """
-    Context manager for the database session
-    """
-    with get_db_session(config=conf) as db_session:
-        tracking_session = db_session
-        yield tracking_session
 
 
 if __name__ == '__main__':
