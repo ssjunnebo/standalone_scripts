c49b324311feea3abde74e70f713bda8fc2994f2
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2019-10-03 10:10:23 +0200

    minor fixes

--- start_sarek/start_sarek.py
+++ start_sarek/start_sarek.py
@@ -11,12 +11,12 @@ import subprocess
 
 
 @click.command()
-@click.option('--genome', type=click.Choice(['GRCh37', 'GRCh38']),
+@click.option("--genome", type=click.Choice(["GRCh37", "GRCh38"]),
               required=True,
               help="Run Sarek with GRCh37 or GRCh38")
-@click.option('--no_submit_jobs', is_flag=True,
+@click.option("--no_submit_jobs", is_flag=True,
               help="Only generate the tsv files and sbatch scripts")
-@click.argument('project_id')
+@click.argument("project_id")
 
 
 def start_sarek(project_id, genome, no_submit_jobs):
@@ -25,36 +25,38 @@ def start_sarek(project_id, genome, no_submit_jobs):
     """
     try:
         charon_connection = CharonSession()
-        sample_entries = charon_connection.project_get_samples(project_id).get("samples", {})
-    except CharonError: # TODO: check that this works
+        sample_entries = charon_connection.project_get_samples(project_id).get("samples", {}) # This should chek if the project exists
+    except:
         print("There was an issue connecting to charon, stopping.")
+    if not sample_entries:
+        print("Could not find any samples for project " + project_id + ". Stopping")
+        return
 
     try:
         config = load_yaml_config(locate_ngi_config())
-        project_base_path = config['start_sarek']['project_base_path']
-        template_path = config['start_sarek']['sbatch_template']
+        project_base_path = config["start_sarek"]["project_base_path"]
+        template_path = config["start_sarek"]["sbatch_template"]
     except:
         print("Issue reading the ngi config file, stopping")
 
     for sample_entry in sample_entries:
         sample_name = sample_entry.get("sampleid")
         sample_status = sample_entry.get("analysis_status")
-        if sample_status == 'TO_ANALYZE':
+        if sample_status == "TO_ANALYZE":
             sample_data_paths = get_fastq_files(project_base_path, project_id, sample_name)
 
-            if sample_data_paths:    # TODO: this will only indicate if the R1 files exsist. Should also look for the R2 files somewhere
+            if sample_data_paths:
                 try:
-                    analysis_path = os.path.join(project_base_path, 'ANALYSIS', project_id, 'sarek_ngi', sample_name)
+                    analysis_path = os.path.join(project_base_path, "ANALYSIS", project_id, "sarek_ngi", sample_name)
                     make_analysis_dir(analysis_path)
 
-                    tsv_file_path = os.path.join(analysis_path, sample_name + '.tsv')
+                    tsv_file_path = os.path.join(analysis_path, sample_name + ".tsv")
                     make_tsv(sample_name, project_id, sample_data_paths, tsv_file_path)
 
                     sbatch_file_path = os.path.join(analysis_path, "run_germline" + sample_name + ".sbatch")
                     make_sbatch_script(sample_name, project_id, genome, sbatch_file_path, template_path)
                 except:
                     print("Issue setting up Sarek run for sample " + sample_name + ". Stopping.")
-                    break
 
                 if no_submit_jobs:
                     print("Generated files. Not submitting jobs.")
@@ -68,7 +70,7 @@ def start_sarek(project_id, genome, no_submit_jobs):
                         break
 
                     try:
-                        charon_connection.sample_update(project_id, sample_name, analysis_status='UNDER_ANALYSIS')
+                        charon_connection.sample_update(project_id, sample_name, analysis_status="UNDER_ANALYSIS")
                         print("Updated analysis status in charon for " + sample_name)
                     except:
                         print("Issue updating charon with job status.")
@@ -83,7 +85,7 @@ def get_fastq_files(project_path, project_id, sample_id):
     """Given a project and sample ID, return a list of paths to the fastq files.
     If the files don't exist, an empty list is returned.
     """
-    path_pattern = os.path.join(project_path, 'DATA', project_id, sample_id, '*/*/*R1*.gz')
+    path_pattern = os.path.join(project_path, "DATA", project_id, sample_id, "*/*/*R1*.gz")
     sample_fastq_paths = glob.glob(path_pattern)
 
     return sample_fastq_paths
@@ -91,19 +93,23 @@ def get_fastq_files(project_path, project_id, sample_id):
 
 def make_tsv(sample, project, sample_fastq_paths, tsv_file_path):
     """Given sample information, generate a tsv file for input to Sarek"""
+    if os.path.isfile(tsv_file_path):
+        os.remove(tsv_file_path)
     for index, frw_fastq in enumerate(sample_fastq_paths, 1):
-        rev_fastq = frw_fastq.replace('_R1_','_R2_')
+        rev_fastq = frw_fastq.replace("_R1_","_R2_")
         lane_nr = str(index)
-        if os.path.isfile(frw_fastq) and os.path.isfile(frw_fastq):
-            with open(tsv_file_path, 'a') as f:                       # TODO: If the file exists, it will just get appended to. Remove first?
-                f.write(sample + '\t'
-                        + "ZZ" + '\t'   # TODO: implement gender option for somatic
-                        + '0' + '\t'
-                        + sample + '\t'
-                        + sample + '_' + lane_nr + '\t'
-                        + frw_fastq + '\t'
-                        + rev_fastq + '\n')
-    print('Writing tsv file in ' + tsv_file_path)
+        if os.path.isfile(frw_fastq) and os.path.isfile(rev_fastq):
+            with open(tsv_file_path, "a") as f:
+                f.write(sample + "\t"
+                        + "ZZ" + "\t"   # TODO: implement gender option for somatic
+                        + "0" + "\t"
+                        + sample + "\t"
+                        + sample + "_" + lane_nr + "\t"
+                        + frw_fastq + "\t"
+                        + rev_fastq + "\n")
+        else:
+            print("Issue locating one or more fastq files: \n" + frw_fastq + "\n" + rev_fastq + "\nSkipping lane.")
+    print("Writing tsv file in " + tsv_file_path)
 
     return
 
@@ -121,12 +127,12 @@ def make_sbatch_script(sample, project_id, reference, sbatch_path, template):
     placeholders = ("PROJECT_ID", "SAMPLE_ID", "REFERENCE")
     replacements = (project_id, sample, reference)
     with open(template, "r") as infile:
-        with open(sbatch_path, 'w') as outfile:
+        with open(sbatch_path, "w") as outfile:
             for line in infile:
                 for placeholder, replacement in zip(placeholders, replacements):
                     line = line.replace(placeholder, replacement)
                 outfile.write(line)
-    print('Writing sbatch script for ' + sample)
+    print("Writing sbatch script " + sbatch_path)
 
     return
 
@@ -141,12 +147,12 @@ def submit_sbatch_job(sample, analysis_dir, sbatch_path):
 
     process_out, process_err = process_handle.communicate()
     try:
-        slurm_job_id = re.match(r'Submitted batch job (\d+)', process_out).groups()[0]
+        slurm_job_id = re.match(r"Submitted batch job (\d+)", process_out).groups()[0]
     except AttributeError:
-        raise RuntimeError('Could not submit sbatch file "{}": '
-                           '{}'.format(sbatch_path, process_err))
+        raise RuntimeError("Could not submit sbatch file '{}': "
+                           "{}".format(sbatch_path, process_err))
     return int(slurm_job_id)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start_sarek()
